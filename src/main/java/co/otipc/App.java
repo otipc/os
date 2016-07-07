18dcd01595961bcb535c668f16a21ade35b3a040
otipc <otipc@163.com>
2016-07-07 14:57:09 +0800

    add jsqlparser

--- src/main/java/co/otipc/App.java
+++ src/main/java/co/otipc/App.java
@@ -1,12 +1,24 @@
 package co.otipc;
 
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserManager;
+import net.sf.jsqlparser.statement.Statement;
+
+import java.io.StringReader;
+
 /**
  * Created by Chaoguo.Cui on 16/7/7.
  */
 public class App {
-  public static void main(String[] args){
+  public static void main(String[] args) throws JSQLParserException {
 
     System.out.println();
+
+    CCJSqlParserManager pm = new CCJSqlParserManager();
+
+    String sql = "";
+
+    Statement stat = pm.parse(new StringReader(sql));
 
   }
 }
